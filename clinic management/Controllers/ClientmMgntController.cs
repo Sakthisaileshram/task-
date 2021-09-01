af5dc7be2bde2e06d295f51fbd13c86665da1b85
Sakthisaileshram <88082777+Sakthisaileshram@users.noreply.github.com>
2021-09-01 15:16:22 +0530

    Add files via upload

---
+++ clinic management/Controllers/ClientmMgntController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClientManagement.Models;
+using System.Data;
+namespace ClientManagement.Controllers
+{
+    public class ClientmMgntController : Controller
+    {
+        // GET: ClientmMgnt
+
+        public ActionResult login()
+        {
+            return View("login");
+        }
+        
+
+        public ActionResult Verify(FormCollection frm, string action)
+        {
+            ClientModel cmobj = new ClientModel();
+            DataTable dt = cmobj.Getdata();
+            string uname = frm["TB_user_name"];
+            string password = frm["TB_password"];
+            if (action == "Submit")
+            {
+         
+                for (int i = 0;i<=dt.Rows.Count; i++)
+                {
+                  
+                    if (uname == dt.Rows[i]["UserName"].ToString() && password == dt.Rows[i]["Pword"].ToString())
+                    {
+                        return View("Home");
+                    }
+
+                    else
+                    { return View("login"); }
+
+                }
+            }
+
+           
+
+
+            return View("login");
+
+          
+        }
+        public ActionResult home()
+        { return View("Home"); }
+        public ActionResult doc()
+        {
+           ClientModel cmobj = new ClientModel();
+           DataTable dt = cmobj.GetDoctordata();
+            return View("doctor",dt);
+        }
+        public ActionResult Docinsert()
+        { return View("doctor"); }
+        public ActionResult Idoctor(FormCollection dfrm, string action)
+        {
+           
+          
+
+
+            if (action == "Submit")
+            {
+               ClientModel cmobj = new ClientModel();
+                string fname = dfrm["TB_f_name"];
+                string lname = dfrm["TB_l_name"];
+                string sex = dfrm["TB_gen"];
+                string sppz = dfrm["TB_spezz"];
+                string Vhrss = dfrm["TB_vhr"];
+                int instatus = cmobj.insert_doctorData(fname,lname,sex,sppz,Vhrss);
+             
+                return RedirectToAction ("doc");
+
+            }
+            else 
+            {
+               
+                return View("doc"); 
+            }
+
+           
+
+        }
+
+        public ActionResult Pat()
+        {
+            ClientModel cmobj = new ClientModel();
+            DataTable dt = cmobj.GetPatientdata();
+            return View("patient", dt);
+        }
+        public ActionResult patient(FormCollection pfrm,string action)
+        {
+            if (action == "Submit")
+            {
+                ClientModel cmobj = new ClientModel();
+                string fname = pfrm["TB_Pf_name"];
+                string lname = pfrm["TB_Pl_name"];
+                string sex = pfrm["TB_Pgen"];
+                string age = pfrm["TB_age"];
+                string dob = pfrm["TB_dob"];
+                int instatus = cmobj.insert_PatientData(fname, lname, sex, age, dob);
+
+                // return View("patient");
+                return RedirectToAction("Pat");
+
+            }
+            else
+            {
+
+                return View("patient");
+            }
+
+
+
+        }
+
+        public ActionResult app(ClientModel cmobj)
+
+        {
+            //ClientModel cmobj = new ClientModel();
+            cmobj.Doclist = cmobj.populateDocklist();
+            var selectedItem = cmobj.Doclist.Find(p => p.Value == cmobj.firstName);
+            // DataTable dt = cmobj.GetAppointmentdata();
+            return View(cmobj);
+        }
+        public ActionResult appointment(FormCollection appfrm, string action)
+        {
+            ClientModel cmobj = new ClientModel();
+             DataTable dt = cmobj.GetAppointmentdata();
+
+            if (action == "Submit")
+            {
+               
+                string id = appfrm["id"];
+                string Spez = appfrm["dept"];
+                //string dname = appfrm["TB_Pl_name"];
+                string Vday = appfrm["TB_Vdate"].ToString();
+                string time = appfrm["TB_AppTime"].ToString();
+                //  int instatus = cmobj.insert_appointmentData(Spez, dname, Vday, time);
+                int instatus = cmobj.insert_appointmentData( Convert.ToInt32(id),Spez, Vday, time);
+                //return View("appointment");
+                return RedirectToAction("app");
+
+            }
+            else
+            {
+                
+                return View("appointment");
+
+            }
+        }
+    }
+}
+   
---
+++ clinic management/Models/ClientModel.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+
+
+namespace ClientManagement.Models
+{
+
+    public class ClientModel
+    {
+        SqlConnection con;
+
+
+        public ClientModel()
+        {
+            con = new SqlConnection("Data source =LAPTOP-2MS5KE03; database =Clientmgmt; integrated security=true");
+
+        }
+
+        public DataTable Getdata()
+        {
+            string selqry = "select UserName,Pword from loginDeets";
+            SqlDataAdapter da = new SqlDataAdapter(selqry, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        public DataTable GetDoctordata()
+        {
+            string selqry = "select firstName,lastName,sex,specialization,visiting_hours from doctor";
+            SqlDataAdapter da = new SqlDataAdapter(selqry, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+        public int insert_doctorData(string fn, string ln, string gen, string spez, string vhr)
+        {
+            string insDqry = "insert into doctor values (@fn,@ln,@gen,@spez,@vhr)";// values(@Fname,@Lname,@sez,@spz,@Vhours)";
+            SqlCommand cmd = new SqlCommand(insDqry, con);
+            cmd.Parameters.AddWithValue("@fn", fn);
+            cmd.Parameters.AddWithValue("@ln", ln);
+            cmd.Parameters.AddWithValue("@gen", gen);
+            cmd.Parameters.AddWithValue("@spez", spez);
+            cmd.Parameters.AddWithValue("@vhr", vhr);
+            con.Open();
+            return cmd.ExecuteNonQuery();
+            con.Close();
+        }  
+            
+        
+
+        public DataTable GetPatientdata()
+        {
+            string selqry = "select patient_id,firstName,lastName,sex,Age,DOB from tbl_Patient";
+            SqlDataAdapter da = new SqlDataAdapter(selqry, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+            public int insert_PatientData(string fn, string ln, string gen, string age, string dob)
+            {
+            string insDqry = "insert into tbl_Patient values (@fn,@ln,@gen,@age,@dob)";
+            SqlCommand cmd = new SqlCommand(insDqry, con);
+            cmd.Parameters.AddWithValue("@fn", fn);
+            cmd.Parameters.AddWithValue("@ln", ln);
+            cmd.Parameters.AddWithValue("@gen", gen);
+            cmd.Parameters.AddWithValue("@age",age );
+            cmd.Parameters.AddWithValue("@dob", dob);
+            con.Open();
+            return cmd.ExecuteNonQuery();
+            con.Close();
+
+
+            }
+        public DataTable GetAppointmentdata()
+        {
+          //  string selqry = "select Patient_id,Specialization,visiting_day,time_slot from Appointment";
+            string qry = "select ap.Patient_id,ap.Specialization,doctorName,visiting_day,time_slot from Appointment ap join doctor doc on doc.specialization = ap.Specialization join tbl_patient pat on pat.patient_id = ap.Patient_id";
+            SqlDataAdapter da = new SqlDataAdapter(qry, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+        public List<SelectListItem> Doclist { get; set; }
+        public string firstName { get; set; }
+
+        public List<SelectListItem> populateDocklist()
+        {
+            List<SelectListItem> dlist = new List<SelectListItem>();
+            string selqry = "select firstName from doctor";
+            SqlCommand cmd = new SqlCommand(selqry, con);
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+                Doclist.Add(new SelectListItem { Text = rdr["firstName"].ToString(), Value= rdr["firstName"].ToString() }) ;
+
+            con.Close();
+            return dlist;
+        }
+
+        // public int insert_appointmentData(string sp, string dn, string vd, string t)
+        public int insert_appointmentData(int id,string sp, string vd, string t)
+        {
+           // string insDqry = "insert into Appointment values (@sp,@dn,@vd,@t)";
+            string insDqry = "insert into Appointment values (@sp,@vd,@t)";
+            SqlCommand cmd = new SqlCommand(insDqry, con);
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Parameters.AddWithValue("@sp", sp);
+            //cmd.Parameters.AddWithValue("@dn", dn);
+            cmd.Parameters.AddWithValue("@vd", vd);
+            cmd.Parameters.AddWithValue("@t", t);
+            
+            con.Open();
+            return cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+    }
+    }
